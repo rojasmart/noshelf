c251e88233e03f3d7058d7795185ef56292bb4d3
rojasmart <rogeriosvaldo@gmail.com>
2026-02-15 22:40:56 +0000

    --fix: update DATABASE_URL to use environment variable with default value

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,10 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = "postgresql://noshelf_user:senha123@localhost/noshelf"
-
+# Busca a URL do banco de dados da variável de ambiente, com um valor padrão
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://noshelf_user:Carminauriel1984/@localhost/noshelf")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
